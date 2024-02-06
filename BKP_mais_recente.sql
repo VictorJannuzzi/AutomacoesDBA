23d5e5210c3a9c0e5e2cbcbca7ff8fe430ec96cd
Victor Hugo <159100493+VictorJannuzzi@users.noreply.github.com>
2024-02-06 17:58:16 -0300

    Create BKP_mais_recente.sql

---
+++ BKP_mais_recente.sql
@@ -0,0 +1,30 @@
+WITH CTE AS(
+SELECT 
+   CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
+   msdb.dbo.backupset.database_name, 
+   msdb.dbo.backupset.backup_start_date, 
+   msdb.dbo.backupset.backup_finish_date, 
+   msdb.dbo.backupset.expiration_date, 
+   CASE msdb..backupset.type 
+      WHEN 'D' THEN 'Database' 
+      WHEN 'L' THEN 'Log' 
+      END AS backup_type, 
+   msdb.dbo.backupset.backup_size, 
+   msdb.dbo.backupmediafamily.logical_device_name, 
+   msdb.dbo.backupmediafamily.physical_device_name, 
+   msdb.dbo.backupset.name AS backupset_name, 
+   msdb.dbo.backupset.description 
+FROM
+   msdb.dbo.backupmediafamily 
+   INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE
+	type = 'D' -- aqui você filtra pelo tipo de backup que você quer utilizar
+   --and (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 3) -- no 2 você põe quantos dias de histórico até o atual
+)
+  SELECT *
+FROM (
+  SELECT ROW_NUMBER() OVER (PARTITION BY database_name ORDER BY backup_start_date DESC) AS rank, *
+  FROM cte
+) t
+WHERE rank = 1 
+ORDER BY backup_start_date DESC
